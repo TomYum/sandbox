e8ef5bfa639c3e934cb4efd677e0f25cec462f59
Tom.Yum. <hart2005@gmail.com>
2014-10-08 13:38:55 +0400

    Переписан весь класс. Добавлена поддержка нескольких подключекний к БД

--- usertest.php
+++ usertest.php
@@ -10,13 +10,31 @@ require_once './classes/db.class.php';
 require_once './classes/Session.class.php';
 require_once './classes/user.class.php';
 
-
+/*
 Db::$database = 'sandbox_git_bd';
 Db::$host = 'localhost';
 Db::$user = 'root';
 Db::$password = '';
+/**/
 
-$session = Session::getInstance();
-//echo $session->getSid();
-$session->clean();
+/** CONFIG
+ **/
+Session::setDbName('sandbox_git_bd');
 
+
+
+$db = Db::getInstance();
+$db->setDbConfig('sandbox_git_bd', 'localhost', 'root');
+$db->setDbConfig('u304043', 'localhost', 'root');
+
+try{
+    $session = Session::getInstance();
+    echo $session->getSid();
+    $session->clean();
+}  catch (dbException $e){
+    echo $e->getMessage();
+}catch (Exception $e){
+    echo $e->getMessage();
+}
+
+/**/
